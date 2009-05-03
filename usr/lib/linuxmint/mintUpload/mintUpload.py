434947881f8f91c0224178a87a3b6d9e3ae733cd
Philip Morrell <ubuntu.emorrp1@mamber.net>
2009-05-03 12:16:21 +0100

    fixing indentation errors

--- usr/lib/linuxmint/mintUpload/mintUpload.py
+++ usr/lib/linuxmint/mintUpload/mintUpload.py
@@ -8,50 +8,50 @@
 # of the License.
  
 try:
-    import os
-    from mintUploadCore import *
+	import os
+	from mintUploadCore import *
 except:
-    print "You do not have all the dependencies!"
-    sys.exit(1)
+	print "You do not have all the dependencies!"
+	sys.exit(1)
 
 # i18n
 gettext.install("messages", "/usr/lib/linuxmint/mintUpload/locale")
 
 def cliUpload(servicename, filename):
-    services = read_services()
-    for service in services:
-        if service['name'] == servicename:
+	services = read_services()
+	for service in services:
+		if service['name'] == servicename:
 
-            # Check there is enough space on the service, ignore threading
-            filesize = os.path.getsize(filename)
-            checker = mintSpaceChecker(service, filesize)
-            proceed = checker.run()
+			# Check there is enough space on the service, ignore threading
+			filesize = os.path.getsize(filename)
+			checker = mintSpaceChecker(service, filesize)
+			proceed = checker.run()
 
-            if proceed:
-                # Upload
-                uploader = mintUploader(service, filename)
-                uploader.start()
-            else:
-                raise CustomError(_("Upload failed."))
+			if proceed:
+				# Upload
+				uploader = mintUploader(service, filename)
+				uploader.start()
+			else:
+				raise CustomError(_("Upload failed."))
 
 if __name__ == "__main__":
 
-    if len(sys.argv) < 2:
-        print "need a file to upload!"
-        exit(1)
+	if len(sys.argv) < 2:
+		print "need a file to upload!"
+		exit(1)
 
-    elif sys.argv[1] == "--version":
-        print "mintupload: %s" % commands.getoutput("mint-apt-version mintupload 2> /dev/null")
-        exit(0)
-    elif sys.argv[1] in ["-h","--help"]:
-        print """Usage: mintupload.py path/to/filename"""
-        print """Usage: mintupload.py -cli SERVICE path/to/filename"""
-        exit(0)
-    elif sys.argv[1] == "-cli":
-        if len(sys.argv) == 4:
-            cliUpload(sys.argv[2], sys.argv[3])
-        else:
-            print """Usage: mintupload.py -cli SERVICE path/to/filename"""
+	elif sys.argv[1] == "--version":
+		print "mintupload: %s" % commands.getoutput("mint-apt-version mintupload 2> /dev/null")
+		exit(0)
+	elif sys.argv[1] in ["-h","--help"]:
+		print """Usage: mintupload.py path/to/filename"""
+		print """Usage: mintupload.py -cli SERVICE path/to/filename"""
+		exit(0)
+	elif sys.argv[1] == "-cli":
+		if len(sys.argv) == 4:
+			cliUpload(sys.argv[2], sys.argv[3])
+		else:
+			print """Usage: mintupload.py -cli SERVICE path/to/filename"""
 	else:
 		if len(sys.argv) > 2:
 			print "too many files! using only the first!"
